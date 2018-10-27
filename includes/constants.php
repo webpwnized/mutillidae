9333057c76ca53f2118314a3b56a60cb925ef3ae
webpwnized <webpwnized@gmail.com>
2018-10-26 22:29:35 -0400

    =2.6.71

--- includes/constants.php
+++ includes/constants.php
@@ -3,7 +3,7 @@
 	/* ------------------------------------------
 	 * @VERSION
 	 * ------------------------------------------*/
-	$C_VERSION = "2.6.70";
+	$C_VERSION = "2.6.71";
 	$C_VERSION_STRING = "Version: " . $C_VERSION;
 	$C_MAX_HINT_LEVEL = 1;
 	
