0ae3c330e82fc862fac1e6a7eaa5e45a77c9b6cf
webpwnized <webpwnized@gmail.com>
2019-02-14 14:27:02 -0500

    =2.7.8

--- includes/constants.php
+++ includes/constants.php
@@ -3,7 +3,7 @@
 	/* ------------------------------------------
 	 * @VERSION
 	 * ------------------------------------------*/
-	$C_VERSION = "2.7.7";
+	$C_VERSION = "2.7.8";
 	$C_VERSION_STRING = "Version: " . $C_VERSION;
 	$C_MAX_HINT_LEVEL = 1;
 	
