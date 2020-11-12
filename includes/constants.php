9fb239732b12da597ebd7258341a0f1ceb085544
webpwnized <webpwnized@gmail.com>
2020-11-11 21:15:34 -0500

    2.8.4

--- includes/constants.php
+++ includes/constants.php
@@ -3,7 +3,7 @@
 	/* ------------------------------------------
 	 * @VERSION
 	 * ------------------------------------------*/
-	$C_VERSION = "2.8.3";
+	$C_VERSION = "2.8.4";
 	$C_VERSION_STRING = "Version: " . $C_VERSION;
 	$C_MAX_HINT_LEVEL = 1;
 	
--- javascript/inline-initializers/hints-menu-init.js
+++ javascript/inline-initializers/hints-menu-init.js
@@ -1,13 +1,15 @@
 $(function() {
-	window.document.getElementById("idHintWrapperHeader").addEventListener('click', toggleBody);
-	
-	window.document.getElementById("idHintWrapperHeader").addEventListener('mouseover', function(){
-		this.style.backgroundColor='#cccccc';
-		this.style.color='#ffffff';
-	});
-	
-	window.document.getElementById("idHintWrapperHeader").addEventListener('mouseout', function(){
-		this.style.backgroundColor='#FFFFFF';
-		this.style.color='#000000';
-	});
+	if (window.document.getElementById("idHintWrapperHeader")){
+		window.document.getElementById("idHintWrapperHeader").addEventListener('click', toggleBody);
+		
+		window.document.getElementById("idHintWrapperHeader").addEventListener('mouseover', function(){
+			this.style.backgroundColor='#cccccc';
+			this.style.color='#ffffff';
+		});
+		
+		window.document.getElementById("idHintWrapperHeader").addEventListener('mouseout', function(){
+			this.style.backgroundColor='#FFFFFF';
+			this.style.color='#000000';
+		});
+	};
 });
