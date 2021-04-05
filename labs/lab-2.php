80dca4a4371629c7c7aac701d1bbad81cdb62f9e
webpwnized <webpwnized@gmail.com>
2021-04-05 16:57:58 -0400

    2.8.31: Added Lab 2

---
+++ labs/lab-2.php
@@ -0,0 +1,12 @@
+<?php
+    $lTitle = "Lab 2 - Capturing HTTP Packets with Wireshark";
+    $lQuestion = "Use the Firefox browser to connect to Mutillidae. What is the first word in the user-agent string?";
+    $lChoice_1 = "Firefox";
+    $lChoice_2 = "Gecko";
+    $lChoice_3 = "Linux";
+    $lChoice_4 = "Mozilla";
+    $lChoice_5 = "Trident";
+    $lCorrectAnswer = 4;
+
+    require_once("labs/lab-template.inc");
+?>
