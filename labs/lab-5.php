7b6055625968058fd27ed539f718694d4061caf4
webpwnized <webpwnized@gmail.com>
2021-04-06 10:58:40 -0400

    2.8.32: Added Lab 5

---
+++ labs/lab-5.php
@@ -0,0 +1,12 @@
+    <?php
+    $lTitle = "Lab 5: How the Web Really Works - Bypassing Client-side Security";
+    $lQuestion = "What attribute code causes the Vanishing Text Box input element to vanish?";
+    $lChoice_1 = 'type="hidden"';
+    $lChoice_2 = 'input.hidden="true"';
+    $lChoice_3 = 'readonly="readonly"';
+    $lChoice_4 = 'vanish="true"';
+    $lChoice_5 = 'onmouseover="javascript:this.type=\'hidden\';"';
+    $lCorrectAnswer = 5;
+
+    require_once("labs/lab-template.inc");
+?>
