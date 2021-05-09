91af3a8e1e533bf95b82c2d0674ca8798ade8fce
webpwnized <webpwnized@gmail.com>
2021-05-09 15:45:00 -0400

    2.8.45: Patched Conference Room Lookup page for PHP 7.4

--- conference-room-lookup.php
+++ conference-room-lookup.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	/* LDAP Injection
 	 * Method Tampering
 	 */
@@ -17,7 +17,7 @@
 		return $EncodedString;
 	}
 
-	try {	    	
+	try {
     	switch ($_SESSION["security-level"]){
     		case "0": // This code is insecure. No input validation is performed.
 				$lEnableJavaScriptValidation = FALSE;
@@ -48,37 +48,37 @@
 		if (isset($_POST["default_room_common_name"]) || isset($_REQUEST["default_room_common_name"])) {
 			$lFormSubmitted = TRUE;
 		}// end if
-		
+
 		if ($lFormSubmitted){
-			
+
 			$lProtectAgainstMethodTampering?$lRoomCommonName = $_POST["default_room_common_name"]:$lRoomCommonName = $_REQUEST["default_room_common_name"];
-	    	
-	    	
+
+
 	    	if ($lProtectAgainstLDAPInjection) {
     			/* Protect against LDAP Injection by encoding */
     			$lRoomCommonNameText = encodeForLDAP($lRoomCommonName);
 	    	}else{
 			/* allow LDAP Injection by not encoding output */
-			$lRoomCommonNameText = $lRoomCommonName; 			    		
+			$lRoomCommonNameText = $lRoomCommonName;
 	    	}//end if
-	    	
-		}// end if $lFormSubmitted    	
-    	    		    	    	
+
+		}// end if $lFormSubmitted
+
 	}catch(Exception $e){
 		echo $CustomErrorHandler->FormatError($e, "Error setting up configuration on page conference-lookup.php");
-	}// end try	
+	}// end try
 ?>
 
 <div class="page-title">Conference Room Lookup</div>
 
 <?php include_once (__ROOT__.'/includes/back-button.inc');?>
 <?php include_once (__ROOT__.'/includes/hints/hints-menu-wrapper.inc'); ?>
-    
+
 <!-- BEGIN HTML OUTPUT  -->
 <script type="text/javascript">
 	var onSubmitOfForm = function(/* HTMLForm */ theForm){
 
-		<?php 
+		<?php
 		if($lEnableJavaScriptValidation){
 			echo "var lOSLDAPInjectionPattern = /[;&\*]/;";
 			echo "var lCrossSiteScriptingPattern = /[<>=()]/;";
@@ -87,24 +87,24 @@
 			echo "var lCrossSiteScriptingPattern = /[]/;";
 		}// end if
 		?>
-		
+
 		if(theForm.default_room_common_name.value.search(lOSLDAPInjectionPattern) > -1){
 			alert("Malicious characters are not allowed.\n\nDon\'t listen to security people. Everyone knows if we just filter dangerous characters, injection is not possible.\n\nWe use JavaScript defenses combined with filtering technology.\n\nBoth are such great defenses that you are stopped in your tracks.");
 			return false;
 		}else if(theForm.default_room_common_name.value.search(lCrossSiteScriptingPattern) > -1){
 			alert("Characters used in cross-site scripting are not allowed.\n\nDon\'t listen to security people. Everyone knows if we just filter dangerous characters, injection is not possible.\n\nWe use JavaScript defenses combined with filtering technology.\n\nBoth are such great defenses that you are stopped in your tracks.");
-			return false;			
+			return false;
 		}else{
 			return true;
 		}// end if
 	};// end JavaScript function onSubmitOfForm()
 </script>
 
-<form 	action="index.php?page=conference-room-lookup.php" 
-			method="post" 
-			enctype="application/x-www-form-urlencoded" 
+<form 	action="index.php?page=conference-room-lookup.php"
+			method="post"
+			enctype="application/x-www-form-urlencoded"
 			onsubmit="return onSubmitOfForm(this);"
-			id="idConferenceRoomLookupForm">		
+			id="idConferenceRoomLookupForm">
 	<table>
 		<tr id="id-bad-cred-tr" style="display: none;">
 			<td colspan="2" class="error-message">
@@ -143,8 +143,8 @@
 if ($lFormSubmitted){
 	try{
 		require_once(__SITE_ROOT__ . '/includes/ldap-config.inc');
-		
-		$ldapconn=ldap_connect(LDAP_HOST,LDAP_PORT);
+
+		$ldapconn=ldap_connect(LDAP_HOST, LDAP_PORT);
 		ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
 		ldap_bind($ldapconn, LDAP_BIND_DN, LDAP_BIND_PASSWORD);
 
@@ -154,18 +154,20 @@ if ($lFormSubmitted){
 		$entries = ldap_get_entries($ldapconn, $sr);
 
 		echo '<table><tr><th>These rooms are available</th></tr>';
-			foreach ($entries as $key) {
-				echo '<tr colspan="1"><td>';
-				echo $key['cn'][0];
-				echo '</td></tr>';
-			}
+		foreach ($entries as $key) {
+			if ( is_array($key) && isset($key['cn']) ){
+			    echo '<tr colspan="1"><td>';
+			    echo $key['cn'][0];
+			    echo '</td></tr>';
+			} // end if
+		} // end for each
 		echo '</table></div>';
 
 		$LogHandler->writeToLog("Executed LDAP search on: " . $lRoomCommonNameText);
 
-    	}catch(Exception $e){
-		echo $CustomErrorHandler->FormatError($e, "Input: " . $lRoomCommonNameText);
-    	}// end try
-    	
-}// end if ($lFormSubmitted) 
+	}catch(Exception $e){
+        echo $CustomErrorHandler->FormatError($e, "Input: " . $lRoomCommonNameText);
+	}// end try
+
+}// end if ($lFormSubmitted)
 ?>
