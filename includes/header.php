0c854e96f9851ffbb5758179076be619fad200ac
webpwnized <webpwnized@gmail.com>
2021-09-07 19:46:40 -0400

    2.8.55: Fixed small bug

--- includes/header.php
+++ includes/header.php
@@ -9,24 +9,24 @@
             // DO NOTHING: This is equivalent to using client side security
             $lSecurityLevelMessage = "Security Level: ".$lSecurityLevel." (Client-Side Security)";
             break;
-            
+
         case "2":
         case "3":
         case "4":
         case "5": // This code is fairly secure
             $lSecurityLevelMessage = "Security Level: ".$lSecurityLevel." (Secure)";
             break;
-    }// end switch	
+    }// end switch
 
 	if($_SESSION['loggedin'] == "True"){
 
 	    switch ($lSecurityLevel){
 	   		case "0": // This code is insecure
 	   		case "1": // This code is insecure
-	   			// DO NOTHING: This is equivalent to using client side security		
+	   			// DO NOTHING: This is equivalent to using client side security
 				$logged_in_user = $_SESSION['logged_in_user'];
 			break;
-		    
+
 	   		case "2":
 	   		case "3":
 	   		case "4":
@@ -35,19 +35,19 @@
 	   			// this is HTML encoding because we are outputting data into HTML
 				$logged_in_user = $Encoder->encodeForHTML($_SESSION['logged_in_user']);
 			break;
-	   	}// end switch		
+	   	}// end switch
 
 	   	$lUserID = $_SESSION['uid'];
-	   	
+
 	   	$lUserAuthorizationLevelText = 'User';
-	   	
+
 	   	if ($_SESSION['is_admin'] == 'TRUE'){
 	   		$lUserAuthorizationLevelText = 'Admin';
 	   	}// end if
 
-		$lAuthenticationStatusMessage = 
-			'Logged In ' . 
-			$lUserAuthorizationLevelText . ": " . 
+		$lAuthenticationStatusMessage =
+			'Logged In ' .
+			$lUserAuthorizationLevelText . ": " .
 			'<span class="logged-in-user">'.$logged_in_user.'</span>'.
 			'<a href="index.php?page=edit-account-profile.php&uid='.$lUserID.'">'.
             '<img src="images/edit-icon-20-20.png" /></a>';
@@ -60,8 +60,8 @@
 		$lEnforceSSLLabel = "Drop TLS";
 	}else {
 		$lEnforceSSLLabel = "Enforce TLS";
-	}//end if	
-	
+	}//end if
+
 	$lHintsMessage = "Hints: ".$_SESSION["hints-enabled"];
 
 ?>
@@ -69,12 +69,12 @@
 <html>
 <head>
 	<link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon" />
-	
+
 	<link rel="stylesheet" type="text/css" href="styles/global-styles.css" />
 	<link rel="stylesheet" type="text/css" href="styles/ddsmoothmenu/ddsmoothmenu.css" />
 	<link rel="stylesheet" type="text/css" href="javascript/jQuery/colorbox/colorbox.css" />
 	<link rel="stylesheet" type="text/css" href="styles/gritter/jquery.gritter.css" />
-	
+
 	<script src="javascript/jQuery/jquery.js"></script>
 	<script src="javascript/jQuery/colorbox/jquery.colorbox-min.js"></script>
 	<script src="javascript/ddsmoothmenu/ddsmoothmenu.js"></script>
@@ -97,7 +97,7 @@
 	<tr class="main-table-frame-dark">
 		<td class="main-table-frame-second-bar" colspan="2">
 			<?php /* Note: $C_VERSION_STRING in index.php */
-			    echo $C_VERSION_STRING; 
+			    echo $C_VERSION_STRING;
 			?>
 			<span><?php echo $lSecurityLevelMessage; ?></span>
 			<span><?php echo $lHintsMessage; ?></span>
@@ -116,9 +116,9 @@
 				}// end if
 			?>
 			|
-			<?php 
+			<?php
 				if ($_SESSION['security-level'] == 0){
-					echo '<a href="index.php?do=toggle-hints&page='.$lPage.'">Toggle Hints</a>|';
+					echo '<a href="index.php?do=toggle-hints&page='.$lPage.'">Toggle Hints</a> |';
 				}// end if
 			?>
 			<a href="index.php?do=toggle-security&page=<?php echo $lPage?>">Toggle Security</a>
@@ -160,14 +160,14 @@
 					<br/>
 					Announcements
 				</a>
-			</div>		
+			</div>
 			<div>&nbsp;</div>
 			<div>
-				<a 
-					href="https://www.sans.org/reading-room/whitepapers/application/introduction-owasp-mutillidae-ii-web-pen-test-training-environment-34380" 
+				<a
+					href="https://www.sans.org/reading-room/whitepapers/application/introduction-owasp-mutillidae-ii-web-pen-test-training-environment-34380"
 					target="_blank"
 					title="Whitepaper: Introduction to OWASP Mutillidae II Web Pen Test Training Environment"
-				>			
+				>
 					<img align="middle" alt="Webpwnized Twitter Channel" src="./images/pdf-icon-48-48.png" />
 					<br/>
 					Getting Started
