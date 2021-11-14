5a28a0b5347afdf4722ad8cf24413c7ededadae2
webpwnized <webpwnized@gmail.com>
2021-11-14 13:24:27 -0500

    2.8.63: Patched Lab 25

--- labs/lab-25.php
+++ labs/lab-25.php
@@ -1,7 +1,7 @@
 <?php
     $lLabNumber = 25;
     $lTitle = "Lab 25: Cross-Site Scripting - Creating a Cross-site Script Proof of Concept (PoC)";
-    $lQuestion = "In the particular cross-site script used in the lab, what is the purpose of the script element? &lt;script&gt;alert('XSS');&lt;script&gt;";
+    $lQuestion = "In the particular cross-site script used in the lab, what is the purpose of the script element? &lt;script&gt;alert('XSS');&lt;/script&gt;";
     $lChoice_1 = "The script element provides the padding needed to offset the injection";
     $lChoice_2 = "The element restarts the browser engine";
     $lChoice_3 = "The element initializes the cross-site scripting filters";
