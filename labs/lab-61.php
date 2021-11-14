bd9d365df15c0e1f8edfb2c3efffd04a646786bb
webpwnized <webpwnized@gmail.com>
2021-11-14 16:01:08 -0500

    2.8.65: Added Lab 61

---
+++ labs/lab-61.php
@@ -0,0 +1,13 @@
+<?php
+$lLabNumber = 61;
+$lTitle = "Lab 61: Cross-origin Resource Sharing (CORS) - Pre-Flight Requests";
+$lQuestion = "Using the Cross-origin Resource Sharing (CORS) page to send a PUT request, what is the HTTP method of the Pre-Flight Request?";
+$lChoice_1 = "HEAD";
+$lChoice_2 = "PUT";
+$lChoice_3 = "OPTIONS";
+$lChoice_4 = "GET";
+$lChoice_5 = "POST";
+$lCorrectAnswer = 3;
+
+require_once("labs/lab-template.inc");
+?>
