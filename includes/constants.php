ce97058c80917e103b8dbf0c60efa347c5e0b5b3
webpwnized <webpwnized@gmail.com>
2021-11-14 15:26:28 -0500

    2.8.64: Patched Lab 24

--- includes/constants.php
+++ includes/constants.php
@@ -3,7 +3,7 @@
 	/* ------------------------------------------
 	 * @VERSION
 	 * ------------------------------------------*/
-	$C_VERSION = "2.8.63";
+	$C_VERSION = "2.8.64";
 	$C_VERSION_STRING = "Version: " . $C_VERSION;
 	$C_MAX_HINT_LEVEL = 1;
 
--- labs/lab-24.php
+++ labs/lab-24.php
@@ -1,7 +1,8 @@
 <?php
     $lLabNumber = 24;
     $lTitle = "Lab 24: Open Redirects - Part 2";
-    $lQuestion = "What domain name has been encoded?";
+    $lQuestion = "What domain name has been encoded? %77%77%77%2E%64%75%63%6B%64%75%63%6B%67%6F%2E%63%6F%6D *
+";
     $lChoice_1 = "www.google.com";
     $lChoice_2 = "www.walmart.com";
     $lChoice_3 = "www.duckduckgo.com";
