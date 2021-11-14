559304bacf96a2743f3879d03f535bdac78ce5a8
webpwnized <webpwnized@gmail.com>
2021-11-14 16:17:19 -0500

    2.8.66: Patched formatting issues in the lab hints files

--- includes/constants.php
+++ includes/constants.php
@@ -3,7 +3,7 @@
 	/* ------------------------------------------
 	 * @VERSION
 	 * ------------------------------------------*/
-	$C_VERSION = "2.8.65";
+	$C_VERSION = "2.8.66";
 	$C_VERSION_STRING = "Version: " . $C_VERSION;
 	$C_MAX_HINT_LEVEL = 1;
 
