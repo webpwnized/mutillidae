fe9780a11e418afc6b4d2b4c4c05f8f3fc2a9efd
webpwnized <webpwnized@gmail.com>
2022-03-02 19:16:47 -0500

    2.8.78: Incorporated suggestion from Michal P michpcx

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
-<?php 
-	try {	    	
+<?php
+	try {
     	switch ($_SESSION["security-level"]){
     		case "0": // This code is insecure.
 				$lEnableJavaScriptValidation = FALSE;
@@ -7,10 +7,6 @@
 			break;
 
     		case "1": // This code is insecure.
-				$lEnableJavaScriptValidation = TRUE;
-				$lEnableHTMLControls = TRUE;
-			break;
-
 	   		case "2":
 	   		case "3":
 	   		case "4":
@@ -19,51 +15,51 @@
 				$lEnableHTMLControls = TRUE;
     		break;
     	}// end switch
-    	
+
 	} catch(Exception $e){
 		echo $CustomErrorHandler->FormatError($e, "Error setting up configuration.");
-	}// end try	
+	}// end try
 ?>
 
 <script type="text/javascript">
 <!--
-	<?php 
+	<?php
 		if ($_SESSION["loggedin"]=="True") {
 			echo "var l_loggedIn = true;" . PHP_EOL;
 		}else {
 			echo "var l_loggedIn = false;" . PHP_EOL;
 		}// end if
-	
+
 		if (isset($lAuthenticationAttemptResult)){
 			echo "var lAuthenticationAttemptResultFlag = {$lAuthenticationAttemptResult};" . PHP_EOL;
 		}else{
 			echo "var lAuthenticationAttemptResultFlag = -1;".PHP_EOL;
 		}// end if
- 
+
 		if($lEnableJavaScriptValidation){
 			echo "var lValidateInput = \"TRUE\"" . PHP_EOL;
 		}else{
 			echo "var lValidateInput = \"FALSE\"" . PHP_EOL;
-		}// end if		
+		}// end if
 	?>
 
 	function onSubmitOfLoginForm(/*HTMLFormElement*/ theForm){
 		try{
 			if(lValidateInput == "TRUE"){
 				var lUnsafeCharacters = /[`~!@#$%^&*()-_=+\[\]{}\\|;':",./<>?]/;
-				if (theForm.username.value.length > 15 || 
+				if (theForm.username.value.length > 15 ||
 					theForm.password.value.length > 15){
 						alert('Username too long. We dont want to allow too many characters.\n\nSomeone might have enough room to enter a hack attempt.');
 						return false;
 				};// end if
-				
-				if (theForm.username.value.search(lUnsafeCharacters) > -1 || 
+
+				if (theForm.username.value.search(lUnsafeCharacters) > -1 ||
 					theForm.password.value.search(lUnsafeCharacters) > -1){
 						alert('Dangerous characters detected. We can\'t allow these. This all powerful blacklist will stop such attempts.\n\nMuch like padlocks, filtering cannot be defeated.\n\nBlacklisting is l33t like l33tspeak.');
 						return false;
 				};// end if
 			};// end if(lValidateInput)
-			
+
 			return true;
 		}catch(e){
 			alert("Error: " + e.message);
@@ -79,8 +75,8 @@
 
 <div id="id-log-in-form-div" style="display: none; text-align:center;">
 	<form 	action="index.php?page=login.php"
-			method="post" 
-			enctype="application/x-www-form-urlencoded" 
+			method="post"
+			enctype="application/x-www-form-urlencoded"
 			onsubmit="return onSubmitOfLoginForm(this);"
 			id="idLoginForm">
 		<table>
@@ -145,7 +141,7 @@
 				<input class="button" type="button" value="Logout" onclick="document.location='index.php?do=logout'" />
 			</td>
 		</tr>
-	</table>	
+	</table>
 </div>
 
 <script type="text/javascript">
@@ -156,25 +152,25 @@
    	var cAUTHENTICATION_SUCCESSFUL = 3;
    	var cAUTHENTICATION_EXCEPTION_OCCURED = 4;
    	var cUSERNAME_OR_PASSWORD_INCORRECT = 5;
-   	
+
    	var lMessage = "";
    	var lAuthenticationFailed = "FALSE";
-   	
+
 	switch(lAuthenticationAttemptResultFlag){
-   		case cACCOUNT_DOES_NOT_EXIST: 
+   		case cACCOUNT_DOES_NOT_EXIST:
    	   		lMessage="Account does not exist"; lAuthenticationFailed = "TRUE";
    	   		break;
-   		case cPASSWORD_INCORRECT: 
-   	   		lMessage="Password incorrect"; lAuthenticationFailed = "TRUE"; 
+   		case cPASSWORD_INCORRECT:
+   	   		lMessage="Password incorrect"; lAuthenticationFailed = "TRUE";
    	   		break;
-   		case cNO_RESULTS_FOUND: 
-   	   		lMessage="No results found"; lAuthenticationFailed = "TRUE"; 
+   		case cNO_RESULTS_FOUND:
+   	   		lMessage="No results found"; lAuthenticationFailed = "TRUE";
    	   		break;
-   		case cAUTHENTICATION_EXCEPTION_OCCURED: 
-   	   		lMessage="Exception occurred"; lAuthenticationFailed = "TRUE"; 
+   		case cAUTHENTICATION_EXCEPTION_OCCURED:
+   	   		lMessage="Exception occurred"; lAuthenticationFailed = "TRUE";
    		break;
-   		case cUSERNAME_OR_PASSWORD_INCORRECT: 
-   	   		lMessage="Username or password incorrect"; lAuthenticationFailed = "TRUE"; 
+   		case cUSERNAME_OR_PASSWORD_INCORRECT:
+   	   		lMessage="Username or password incorrect"; lAuthenticationFailed = "TRUE";
    		break;
    	};
 
@@ -188,6 +184,6 @@
 		document.getElementById("id-log-out-div").style.display="none";
 	}else{
 		document.getElementById("id-log-in-form-div").style.display="none";
-		document.getElementById("id-log-out-div").style.display="";		
-	}// end if l_loggedIn	
+		document.getElementById("id-log-out-div").style.display="";
+	}// end if l_loggedIn
 </script>
