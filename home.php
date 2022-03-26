2e8c64dfdea3fe577cc0163fc44cce9b8de799f2
cristian-rincon <cristian.o.rincon.b@gmail.com>
2022-03-25 20:20:41 -0500

    :construction: refactoring home page

--- home.php
+++ home.php
@@ -16,70 +16,112 @@
 	}// end if
 ?>
 
-<div style=" width: 750px; overflow: hidden;">
-<?php include_once (__ROOT__.'/includes/hints/hints-menu-wrapper.inc'); ?>
-<span style="float: right">
-	<img src="images/arrow-45-degree-left-up.png" style="margin-right: 5px" />
-	<span class="label" style="float: right;">TIP:&nbsp;
-	<span style="float: right; text-align: center;">Click 
-	<span style="color: blue;font-style: italic;">Hint and Videos</span><br/>on each page</span></span>
-</span>
+<div class="row">
+	
+	<div class="col-md-4 offset-md-8">
+		<div class="alert alert-info alert-dismissible fade show" role="alert">
+		<?php include_once (__ROOT__.'/includes/hints/hints-menu-wrapper.inc'); ?>
+		<span class="material-icons">
+		arrow_upward
+		</span>
+		TIP: Click <strong><em>Hint and Videos</em></strong> on each page
+			<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+		</div>
+
+	</div>
 </div>
 
-<table style="margin:0px;margin-top:5px;">
- 	<tr>
-	    <td>
+<!-- Section - Row 1 -->
+
+<div class="row">
+	<div class="col">
+		<div class="card">
+			<div class="card-body">
 			<a title="Usage Instructions" href="./index.php?page=documentation/usage-instructions.php">
 				<img alt="What Should I Do?" align="middle" src="./images/question-mark-40-61.png" />
 				What Should I Do?
 			</a>
-		</td>
-		<td>
+			</div>
+		</div>
+	</div>
+	<div class="col">
+		<div class="card">
+			<div class="card-body">
 			<?php include_once './includes/help-button.inc';?>
-		</td>
-	</tr>
-	<tr><td>&nbsp;</td></tr>
-	<tr>
-		<td>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Section - Row 2 -->
+<div class="row py-3">
+	<div class="col">
+		<div class="card">
+			<div class="card-body">
 			<a href="./index.php?page=./documentation/vulnerabilities.php">
 				<img alt="Help" align="middle" src="./images/siren-48-48.png" />
 				Listing of vulnerabilities
 			</a>
-		</td>
-		<td>
+			</div>
+		</div>
+	</div>
+	<div class="col">
+		<div class="card">
+			<div class="card-body">
 			<a href="http://www.youtube.com/user/webpwnized" target="_blank">
 			<img align="middle" alt="Webpwnized YouTube Channel" src="./images/youtube-play-icon-40-40.png" />
 				Video Tutorials
 			</a>
-		</td>
-	</tr>
-	<tr><td>&nbsp;</td></tr>
-	<tr>
+			</div>
+		</div>
+	</div>
+</div>
 
-		<td>
+<!-- Section - Row 3 -->
+
+<div class="row py-3">
+	<div class="col">
+		<div class="card">
+			<div class="card-body">
 			<a href="https://twitter.com/webpwnized" target="_blank">
 				<img align="middle" alt="Webpwnized Twitter Channel" src="./images/twitter-bird-48-48.png" />
 				Release Announcements
 			</a>
-		</td>
-		<td class="label" title="Latest Version">
+			</div>
+		</div>
+	</div>
+	<div class="col">
+		<div class="card">
+			<div class="card-body">
 			<img alt="Latest Version" align="middle" src="./images/installation-icon-48-48.png" />
 			<a title="Latest Version" href="https://github.com/webpwnized/mutillidae" target="_blank">Latest Version</a>
-		</td>
-	</tr>
-	<tr><td>&nbsp;</td></tr>
-	<tr>
-		<td>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Section - Row 4 -->
+<div class="row py-3">
+	<div class="col">
+		<div class="card">
+			<div class="card-body">
 			<a href="documentation/mutillidae-test-scripts.txt" target="_blank">
 				<img alt="Helpful hints and scripts" align="middle" src="./images/help-icon-48-48.png" />
 				Helpful hints and scripts
 			</a>
-		</td>
-		<td class="label" title="Mutillidae LDIF File">
+			</div>
+		</div>
+	</div>
+	<div class="col">
+		<div class="card">
+			<div class="card-body">
 			<a href="configuration/openldap/mutillidae.ldif" target="_blank">
 				<img align="middle" alt="Mutillidae LDIF File" src="./images/ldap-server-48-59.png" />
 				Mutillidae LDIF File
 			</a>
-		</td>
-	</tr>
-</table>
+			</div>
+		</div>
+	</div>
+</div>
+
+
--- includes/header.php
+++ includes/header.php
@@ -73,6 +73,8 @@
 	<title>OWASP Mutillidae II</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
 	<!-- End custom implementation head -->
 	<link rel="stylesheet" type="text/css" href="styles/global-styles.css" />
 	<link rel="stylesheet" type="text/css" href="styles/ddsmoothmenu/ddsmoothmenu.css" />
