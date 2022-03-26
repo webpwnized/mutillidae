172b4295b57f88be16376d80e4ad96d47f45fabc
cristian-rincon <cristian.o.rincon.b@gmail.com>
2022-03-26 02:41:43 -0500

    :construction: user-info view refactored

--- show-log.php
+++ show-log.php
@@ -42,87 +42,101 @@
 	$lQueryResult = $SQLQueryHandler->getHitLogEntries();
 ?>
 
-<div class="page-title">Log</div>
 
-<?php include_once (__ROOT__.'/includes/back-button.inc');?>
-<?php include_once (__ROOT__.'/includes/hints/hints-menu-wrapper.inc'); ?>
 
-<style>
-	#idLogRecords tr td{
-		border: 1px solid black;
-	}
-	#idLogRecords{
-		width: 100%;
-	}
-	img{
-		vertical-align:middle;
-	}
-</style>
-<table class="results-table" id="idLogRecords">
-<tr class="report-header">
-	<td colspan="10">	
-		<span>
-			<img width="32px" height="32px" src="./images/information-icon-64-64.png" />
-			<?php echo $lQueryResult->num_rows; ?> log records found
-		</span>
-		<span title="Click to refresh log file" onclick="document.location.href=document.location.href.replace('&deleteLogs=deleteLogs','').replace('&popUpNotificationCode=LFD1','').concat('&popUpNotificationCode=LFR1');" style="cursor: pointer;margin-left:35px;margin-right:35px;white-space:nowrap;font-weight:bold;">
-			<img width="32px" height="32px" src="./images/refresh-button-48px-by-48px.png" />
-			Refresh Logs
-		</span>
-		<span title="Click to delete log file" onclick="document.location='./index.php?page=show-log.php&deleteLogs=deleteLogs&popUpNotificationCode=LFD1';" style="cursor: pointer;white-space:nowrap;font-weight:bold;">
-			<img width="32px" height="32px" src="./images/delete-icon-48-48.png" />
-			Delete Logs
-		</span>
-	</td>
-</tr>		
-<tr class="report-header">
-    <td style="font-weight:bold;">Hostname</td>
-    <td style="font-weight:bold;">IP</td>
-    <td style="font-weight:bold;">Browser Agent</td>
-    <td style="font-weight:bold;">Message</td>
-    <td style="font-weight:bold;">Date/Time</td>
-</tr>
+<div class="row">
+	<?php include_once (__ROOT__.'/includes/back-button.inc');?>
+	<?php include_once (__ROOT__.'/includes/hints/hints-menu-wrapper.inc'); ?>
+</div>
 
-<?php
-	try{// to draw table		
+<div class="row">
+	<div class="col-md-8 offset-md-2 text-center">
+		<h2>Log</h2>
+	</div>
+</div>
+<div class="card my-3">
+	<div class="card-body">
+		<div class="row d-flex justify-content-between text-center">
+			<div class="col">
+				<span>
+					<span class="material-icons align-middle text-warning">
+					info
+					</span>
+					<?php echo $lQueryResult->num_rows; ?> log records found
+				</span>
+			</div>
+			<div class="col">
+				<span title="Click to refresh log file" onclick="document.location.href=document.location.href.replace('&deleteLogs=deleteLogs','').replace('&popUpNotificationCode=LFD1','').concat('&popUpNotificationCode=LFR1');" style="cursor: pointer;margin-left:35px;margin-right:35px;white-space:nowrap;font-weight:bold;">
+					<span class="material-icons align-middle text-primary">
+					refresh
+					</span>
+					Refresh Logs
+				</span>
+			</div>
+			<div class="col">
+				<span title="Click to delete log file" onclick="document.location='./index.php?page=show-log.php&deleteLogs=deleteLogs&popUpNotificationCode=LFD1';" style="cursor: pointer;white-space:nowrap;font-weight:bold;">
+					<span class="material-icons align-middle text-danger">
+					delete
+					</span>
+					Delete Logs
+				</span>
+			</div>
+		</div>
+	</div>
+</div>
 
-	    if ($lLimitOutput){
-	    	echo '<tr><td class="error-header" colspan="10">Note: DOS defenses enabled. Rows limited to last 20.</td></tr>';
-	    }// end if
+<table class="table table-hover" id="idLogRecords">
+	<caption>Logs list</caption>
+	<thead class="table-dark">
+		
+	<tr class="report-header">
+		<th>Hostname</th>
+		<th>IP</th>
+		<th>Browser Agent</th>
+		<th>Message</th>
+		<th>Date/Time</th>
+	</tr>
+	</thead>
+	<?php
+		try{// to draw table		
 
-	    if($lQueryResult->num_rows > 0){
-		    $lRowNumber = 0;
-		    while($row = $lQueryResult->fetch_object()){
-		    	$lRowNumber++;
-				
-				if(!$lEncodeOutput){
-					$lHostname = $row->hostname;
-					$lClientIPAddress = $row->ip;
-					$lBrowser = $row->browser;
-					$lReferer = $row->referer;
-					$lDate = $row->date;
-				}else{
-					$lHostname = $Encoder->encodeForHTML($row->hostname);
-					$lClientIPAddress = $Encoder->encodeForHTML($row->ip);
-					$lBrowser = $Encoder->encodeForHTML($row->browser);
-					$lReferer = $Encoder->encodeForHTML($row->referer);
-					$lDate = $Encoder->encodeForHTML($row->date);				
-				}// end if
-				
-				echo "<tr>
-						<td>{$lHostname}</td>
-						<td>{$lClientIPAddress}</td>
-						<td>{$lBrowser}</td>
-						<td>{$lReferer}</td>
-						<td>{$lDate}</td>
-					</tr>\n";
-			}//end while $row
-	    }else{
-	    	echo '<tr><td class="warning-message" colspan="10">No Records Found</td></tr>';
-		}//end if
+			if ($lLimitOutput){
+				echo '<tr><td class="error-header" colspan="10">Note: DOS defenses enabled. Rows limited to last 20.</td></tr>';
+			}// end if
+
+			if($lQueryResult->num_rows > 0){
+				$lRowNumber = 0;
+				while($row = $lQueryResult->fetch_object()){
+					$lRowNumber++;
 					
-	} catch (Exception $e) {
-		echo $CustomErrorHandler->FormatError($e, "Error writing log table rows.".$lQueryString);
-	}// end try;
-?>
+					if(!$lEncodeOutput){
+						$lHostname = $row->hostname;
+						$lClientIPAddress = $row->ip;
+						$lBrowser = $row->browser;
+						$lReferer = $row->referer;
+						$lDate = $row->date;
+					}else{
+						$lHostname = $Encoder->encodeForHTML($row->hostname);
+						$lClientIPAddress = $Encoder->encodeForHTML($row->ip);
+						$lBrowser = $Encoder->encodeForHTML($row->browser);
+						$lReferer = $Encoder->encodeForHTML($row->referer);
+						$lDate = $Encoder->encodeForHTML($row->date);				
+					}// end if
+					
+					echo "<tr>
+							<td>{$lHostname}</td>
+							<td>{$lClientIPAddress}</td>
+							<td>{$lBrowser}</td>
+							<td>{$lReferer}</td>
+							<td>{$lDate}</td>
+						</tr>\n";
+				}//end while $row
+			}else{
+				echo '<tr><td colspan="10"><div class="alert alert-info" role="alert">No Records Found</div></td></tr>';
+			}//end if
+						
+		} catch (Exception $e) {
+			echo $CustomErrorHandler->FormatError($e, "Error writing log table rows.".$lQueryString);
+		}// end try;
+	?>
 </table>
