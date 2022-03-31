e95b4d0b08a9257f158f85cda37b3a5fbc93c0be
cristian-rincon <cristian.o.rincon.b@gmail.com>
2022-03-30 20:15:48 -0500

    :memo: removing deprecated code

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <style>
-	a{
+	a {
 		font-weight: bold;
 	}
 </style>
@@ -17,7 +17,7 @@
 ?>
 
 <div class="row">
-	
+
 	<div class="col-md-4 offset-md-8">
 		<div class="alert alert-info alert-dismissible fade show" role="alert">
 			<span class="material-icons align-middle">
@@ -37,24 +37,24 @@
 
 <div class="row">
 	<div class="col">
-		<div class="card">
+		<div class="card shadow-sm">
 			<div class="card-body">
-			<a title="Usage Instructions" href="./index.php?page=documentation/usage-instructions.php">
-				
-				<span class="material-icons md-48 align-middle text-primary">
-					help
-				</span>
-				
+				<a title="Usage Instructions" href="./index.php?page=documentation/usage-instructions.php">
+
+					<span class="material-icons md-48 align-middle text-primary">
+						help
+					</span>
+
 					What Should I Do?
-				
-			</a>
+
+				</a>
 			</div>
 		</div>
 	</div>
 	<div class="col">
-		<div class="card">
+		<div class="card shadow-sm">
 			<div class="card-body">
-			<?php include_once './includes/help-button.inc';?>
+				<?php include_once './includes/help-button.inc';?>
 			</div>
 		</div>
 	</div>
@@ -63,28 +63,28 @@
 <!-- Section - Row 2 -->
 <div class="row py-3">
 	<div class="col">
-		<div class="card">
+		<div class="card shadow-sm">
 			<div class="card-body">
-			<a href="./index.php?page=./documentation/vulnerabilities.php">
-				
-				<span class="material-icons md-48 align-middle text-danger">
-					security
-				</span>
-				Listing of vulnerabilities
-			</a>
+				<a href="./index.php?page=./documentation/vulnerabilities.php">
+
+					<span class="material-icons md-48 align-middle text-danger">
+						security
+					</span>
+					Listing of vulnerabilities
+				</a>
 			</div>
 		</div>
 	</div>
 	<div class="col">
-		<div class="card">
+		<div class="card shadow-sm">
 			<div class="card-body">
-			<a href="http://www.youtube.com/user/webpwnized" target="_blank">
-			
-				<span class="material-icons md-48 align-middle text-danger">
-					ondemand_video
-				</span>
-				Video Tutorials
-			</a>
+				<a href="http://www.youtube.com/user/webpwnized" target="_blank">
+
+					<span class="material-icons md-48 align-middle text-danger">
+						ondemand_video
+					</span>
+					Video Tutorials
+				</a>
 			</div>
 		</div>
 	</div>
@@ -94,24 +94,25 @@
 
 <div class="row py-3">
 	<div class="col">
-		<div class="card">
+		<div class="card shadow-sm">
 			<div class="card-body">
-			<a href="https://twitter.com/webpwnized" target="_blank">
-				<span class="material-icons md-48 align-middle text-warning">
-				new_releases
-				</span>
-				Release Announcements
-			</a>
+				<a href="https://twitter.com/webpwnized" target="_blank">
+					<span class="material-icons md-48 align-middle text-warning">
+						new_releases
+					</span>
+					Release Announcements
+				</a>
 			</div>
 		</div>
 	</div>
 	<div class="col">
-		<div class="card">
+		<div class="card shadow-sm">
 			<div class="card-body">
-			<span class="material-icons md-48 align-middle">
-				settings
-			</span>
-			<a title="Latest Version" href="https://github.com/webpwnized/mutillidae" target="_blank">Latest Version</a>
+				<span class="material-icons md-48 align-middle">
+					settings
+				</span>
+				<a title="Latest Version" href="https://github.com/webpwnized/mutillidae" target="_blank">Latest
+					Version</a>
 			</div>
 		</div>
 	</div>
@@ -120,29 +121,27 @@
 <!-- Section - Row 4 -->
 <div class="row py-3">
 	<div class="col">
-		<div class="card">
+		<div class="card shadow-sm">
 			<div class="card-body">
-			<a href="documentation/mutillidae-test-scripts.txt" target="_blank">
-			<span class="material-icons md-48 align-middle">
-				assignment
-			</span>
-				Helpful hints and scripts
-			</a>
+				<a href="documentation/mutillidae-test-scripts.txt" target="_blank">
+					<span class="material-icons md-48 align-middle">
+						assignment
+					</span>
+					Helpful hints and scripts
+				</a>
 			</div>
 		</div>
 	</div>
 	<div class="col">
-		<div class="card">
+		<div class="card shadow-sm">
 			<div class="card-body">
-			<a href="configuration/openldap/mutillidae.ldif" target="_blank">
-			<span class="material-icons md-48 align-middle text-primary">
-			playlist_add_check_circle
-				</span>
-				Mutillidae LDIF File
-			</a>
+				<a href="configuration/openldap/mutillidae.ldif" target="_blank">
+					<span class="material-icons md-48 align-middle text-primary">
+						playlist_add_check_circle
+					</span>
+					Mutillidae LDIF File
+				</a>
 			</div>
 		</div>
 	</div>
 </div>
-
-
--- javascript/inline-initializers/ddsmoothmenu-init.js
+++ javascript/inline-initializers/ddsmoothmenu-init.js
@@ -3,7 +3,7 @@ $(function() {
 		mainmenuid: "smoothmenu1", //menu DIV id
 		orientation: 'v', //Horizontal or vertical menu: Set to "h" or "v"
 		classname: 'ddsmoothmenu', //class added to menu's outer DIV
-		//customtheme: ["#cccc44", "#cccccc"],
+		// customtheme: ["#cccc44", "#cccccc"],
 		contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
 	});
 });
