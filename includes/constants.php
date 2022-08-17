83a8821bb283bd8389b55a40ebb15b18f6fbd5d7
webpwnized <webpwnized@gmail.com>
2022-08-17 19:16:45 -0400

    2.8.88: Updated lab files

--- includes/constants.php
+++ includes/constants.php
@@ -2,7 +2,7 @@
 	/* ------------------------------------------
 	 * @VERSION
 	 * ------------------------------------------*/
-	$C_VERSION = "2.8.87";
+	$C_VERSION = "2.8.88";
 	$C_VERSION_STRING = "Version: " . $C_VERSION;
 	$C_MAX_HINT_LEVEL = 1;
 
