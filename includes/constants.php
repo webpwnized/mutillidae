96fa1acbe22dfa637484f6ab77f13f7ae54f4df2
webpwnized <webpwnized@gmail.com>
2022-08-16 20:52:13 -0400

    2.8.85: Updated configuration files for docker

--- includes/constants.php
+++ includes/constants.php
@@ -2,7 +2,7 @@
 	/* ------------------------------------------
 	 * @VERSION
 	 * ------------------------------------------*/
-	$C_VERSION = "2.8.84";
+	$C_VERSION = "2.8.85";
 	$C_VERSION_STRING = "Version: " . $C_VERSION;
 	$C_MAX_HINT_LEVEL = 1;
 
