ae4f995b7e03e67c7d3d20011cc3cd849b601ec9
webpwnized <webpwnized@gmail.com>
2022-09-18 15:36:52 -0400

    2.10.6: Updated hydra lab files

--- includes/constants.php
+++ includes/constants.php
@@ -2,7 +2,7 @@
 	/* ------------------------------------------
 	 * @VERSION
 	 * ------------------------------------------*/
-	$C_VERSION = "2.10.5";
+	$C_VERSION = "2.10.6";
 	$C_VERSION_STRING = "Version: " . $C_VERSION;
 	$C_MAX_HINT_LEVEL = 1;
 
