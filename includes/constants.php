56043c6ad33c68cc9a9310b1540acceeb51fa035
webpwnized <webpwnized@gmail.com>
2023-01-25 19:55:41 -0500

    2.11.4: Updated RFI lab instructions

--- includes/constants.php
+++ includes/constants.php
@@ -2,7 +2,7 @@
 	/* ------------------------------------------
 	 * @VERSION
 	 * ------------------------------------------*/
-	$C_VERSION = "2.11.3";
+	$C_VERSION = "2.11.4";
 	$C_VERSION_STRING = "Version: " . $C_VERSION;
 	$C_MAX_HINT_LEVEL = 1;
 
