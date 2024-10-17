3d74d3811ac402307a90088f88859e36e4d215dc
webpwnized <webpwnized@gmail.com>
2024-10-17 19:06:27 -0400

    2.11.22 Update CSP

--- src/includes/header.php
+++ src/includes/header.php
@@ -70,7 +70,7 @@
 	$lHintsMessage = "Hints: ".$_SESSION["hints-enabled"];
 
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/1999/REC-html401-19991224/loose.dtd">
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
