48380ffc010de1145471126777751a6812af577e
webpwnized <webpwnized@gmail.com>
2024-10-18 10:54:49 -0400

    2.11.22 Update JWT user interface

--- src/jwt.php
+++ src/jwt.php
@@ -88,7 +88,7 @@
 	const displayUserDetails = function(pUserInfoJSON) {
 		try {
 			if (pUserInfoJSON && Object.keys(pUserInfoJSON).length > 0) {
-				document.getElementById("idDisplayTable").style.display = "flex";  // Show the table
+				document.getElementById("idDisplayTable").removeAttribute("style");;  // Show the table
 
 				// Safely access properties with default fallbacks
 				addRow('CID', pUserInfoJSON['cid'] ?? 'N/A');
