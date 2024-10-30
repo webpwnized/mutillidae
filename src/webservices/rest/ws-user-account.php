6e9465e55e5f106ec9aba0f0bd89eeb391e32d9c
webpwnized <webpwnized@gmail.com>
2024-10-30 19:12:31 -0400

    2.11.24 Update web services

--- src/webservices/rest/ws-user-account.php
+++ src/webservices/rest/ws-user-account.php
@@ -8,9 +8,9 @@
 	 *   - Access-Control-Allow-Origin: * (or specific domains)
 	 *   - Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS
 	 *   - Access-Control-Allow-Headers: Content-Type, Authorization
-	 * - Expected Response:
-	 *   - Status: 200 OK with JSON response
-	 *   - If method not allowed: 405 Method Not Allowed with allowed methods in response header
+	 *   - Expected Response:
+	 *     - Status: 200 OK with JSON response
+	 *     - If method not allowed: 405 Method Not Allowed with allowed methods in response header
 	 * ------------------------------------------ */
 
 	/* ------------------------------------------
@@ -60,6 +60,8 @@
 	}//end function jsonEncodeQueryResults
 
 	try{
+		$lContentTypeJSON = "Content-Type: application/json";
+
 		// Initialize the SQL query handler
 		$SQLQueryHandler = new SQLQueryHandler(0);
 		$lSecurityLevel = $SQLQueryHandler->getSecurityLevelFromDB();
@@ -79,8 +81,6 @@
 		switch($lVerb){
 			case "GET":
 				if(isset($_GET['username'])){
-					/* Example hack: username=jeremy'+union+select+concat('The+password+for+',username,'+is+',+password),mysignature+from+accounts+--+ */
-
 					$lUsername = $_GET['username'] ?? '';
 					
 					// Fetch data based on the username
@@ -107,136 +107,17 @@
 					}
 					
 					// Output the response as JSON with the correct headers
-					header('Content-Type: application/json');
+					header($lContentTypeJSON);
+					$lArrayResponse['SecurityLevel'] = $lSecurityLevel;
 					echo json_encode($lArrayResponse, JSON_PRETTY_PRINT);
+				} else {
+					http_response_code(400);
+					header($lContentTypeJSON);
+					echo json_encode(["error" => "Username parameter is required", "SecurityLevel" => $lSecurityLevel]);
+				}
+				break;
 
-				}else{
-					header('Content-Type: text/html');
-
-					// Enhanced help content for GET request without parameters
-					echo '
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>User Account Web Service Documentation</title>
-	<style>
-		body {
-			font-family: Arial, sans-serif;
-			line-height: 1.6;
-		}
-		a {
-			text-decoration: none;
-			font-weight: bold;
-		}
-		pre {
-			background-color: #f4f4f4;
-			padding: 10px;
-			border-radius: 5px;
-		}
-	</style>
-</head>
-<body>
-<a href="//mutillidae.localhost/index.php">Back to Home Page</a>
-<br>
-<div>
-	<h2>Welcome to the User Account Web Service</h2>
-	<p>This service allows you to <strong>create, read, update, and delete</strong> user accounts using various HTTP methods.</p>
-	<p><strong>Note:</strong> This service is vulnerable to SQL injection at security level 0. Be cautious when testing or exploring its functionality.</p>
-</div>
-<hr>
-<h3>Supported HTTP Methods</h3>
-<h4>1. GET (Retrieve Data)</h4>
-<p>Use GET requests to retrieve information about one or more accounts.</p>
-<p><strong>Optional Parameter:</strong> <code>username</code> (as a URL parameter)</p>
-<ul>
-	<li>If <code>username=*</code>, all accounts will be returned.</li>
-	<li>If <code>username</code> is specified, details for that user will be returned.</li>
-</ul>
-<strong>Examples:</strong><br>
-<ul>
-	<li>Retrieve a specific user:
-		<pre>
-GET /webservices/rest/ws-user-account.php?username=adrian HTTP/1.1
-Host: mutillidae.localhost
-Accept: application/json
-		</pre>
-	</li>
-	<li>Retrieve all users:
-		<pre>
-GET /webservices/rest/ws-user-account.php?username=* HTTP/1.1
-Host: mutillidae.localhost
-Accept: application/json
-		</pre>
-	</li>
-</ul>
-<hr>
-<h4>2. POST (Create New Account)</h4>
-<p>Use POST requests to create a new user account.</p>
-<p><strong>Required Parameters (POST body):</strong></p>
-<ul>
-	<li><code>username</code>: The username for the new account</li>
-	<li><code>password</code>: The password for the new account</li>
-	<li><code>firstname</code>: User\'s first name</li>
-	<li><code>lastname</code>: User\'s last name</li>
-</ul>
-<p><strong>Optional Parameter:</strong> <code>signature</code> (User\'s signature)</p>
-<strong>Example:</strong><br>
-<pre>
-POST /webservices/rest/ws-user-account.php HTTP/1.1
-Host: mutillidae.localhost
-Content-Type: application/x-www-form-urlencoded
-
-username=john&password=pass123&firstname=John&lastname=Doe&signature=JDoe
-</pre>
-<hr>
-<h4>3. PUT (Create or Update Account)</h4>
-<p>Use PUT requests to <strong>create or update</strong> an existing user account.</p>
-<p><strong>Required Parameters (POST body):</strong> Same as POST</p>
-<p>If the account exists, it will be updated. If not, a new account will be created.</p>
-<strong>Example:</strong><br>
-<pre>
-PUT /webservices/rest/ws-user-account.php HTTP/1.1
-Host: mutillidae.localhost
-Content-Type: application/x-www-form-urlencoded
-
-username=john&password=newpass123&firstname=John&lastname=Doe&signature=JDoeUpdated
-</pre>
-
-<hr>
-
-<h4>4. DELETE (Remove Account)</h4>
-<p>Use DELETE requests to delete an existing user account.</p>
-<p><strong>Required Parameters (POST body):</strong></p>
-<ul>
-	<li><code>username</code>: The username of the account to be deleted</li>
-	<li><code>password</code>: The password for the account</li>
-</ul>
-<strong>Example:</strong><br>
-<pre>
-DELETE /webservices/rest/ws-user-account.php HTTP/1.1
-Host: mutillidae.localhost
-Content-Type: application/x-www-form-urlencoded
-
-username=john&password=newpass123
-</pre>
-<hr>
-<h4>Example Exploits (SQL Injection)</h4>
-<p>This service is vulnerable to SQL injection at security level 0. Example:</p>
-<pre>
-GET /webservices/rest/ws-user-account.php?username=jeremy\'+union+select+concat(\'The+password+for+\',username,\'+is+\',password),mysignature+from+accounts+-- HTTP/1.1
-Host: mutillidae.localhost
-</pre>
-</body>
-</html>
-					';
-					
-				}// end if
-
-			break;
-			case "POST"://create
-
+			case "POST": // create
 				// Fetch POST parameters
 				$lUsername = getPOSTParameter("username", true);
 				$lAccountPassword = getPOSTParameter("password", true);
@@ -271,11 +152,12 @@ Host: mutillidae.localhost
 				}
 				
 				// Set the response header to JSON and output the response
-				header('Content-Type: application/json');
+				header($lContentTypeJSON);
+				$lArrayResponse['SecurityLevel'] = $lSecurityLevel;
 				echo json_encode($lArrayResponse, JSON_PRETTY_PRINT);
+				break;
 
-			break;
-			case "PUT":	//create or update
+			case "PUT": // create or update
 				/* $_POST array is not auto-populated for PUT method. Parse input into an array. */
 				populatePOSTSuperGlobal();
 
@@ -330,10 +212,11 @@ Host: mutillidae.localhost
 				}
 
 				// Set the response header to JSON and output the response
-				header('Content-Type: application/json');
+				header($lContentTypeJSON);
+				$lArrayResponse['SecurityLevel'] = $lSecurityLevel;
 				echo json_encode($lArrayResponse, JSON_PRETTY_PRINT);
+				break;
 
-			break;
 			case "DELETE":
 				/* $_POST array is not auto-populated for DELETE method. Parse input into an array. */
 				populatePOSTSuperGlobal();
@@ -350,7 +233,7 @@ Host: mutillidae.localhost
 					if ($SQLQueryHandler->authenticateAccount($lUsername, $lAccountPassword)) {
 						// Attempt to delete the user
 						$lQueryResult = $SQLQueryHandler->deleteUser($lUsername);
-				
+					
 						if ($lQueryResult) {
 							// Successful deletion
 							$lArrayResponse['Result'] = "Deleted account '$lUsername'.";
@@ -372,16 +255,22 @@ Host: mutillidae.localhost
 				}
 				
 				// Set the response header to JSON and output the response
-				header('Content-Type: application/json');
+				header($lContentTypeJSON);
+				$lArrayResponse['SecurityLevel'] = $lSecurityLevel;
 				echo json_encode($lArrayResponse, JSON_PRETTY_PRINT);
+				break;
 
-			break;
 			default:
-				throw new UnsupportedHttpVerbException($lVerb);
-			break;
+				http_response_code(405);
+				header('Allow: GET, POST, PUT, DELETE, OPTIONS');
+				header($lContentTypeJSON);
+				echo json_encode(["error" => "Method not allowed", "SecurityLevel" => $lSecurityLevel]);
+				break;
 		}// end switch
 
 	} catch (Exception $e) {
+		http_response_code(500);
+		header($lContentTypeJSON);
 		echo $CustomErrorHandler->FormatErrorJSON($e, "Unable to process request to web service ws-user-account");
 	}// end try
 
