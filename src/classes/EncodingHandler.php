bf3f8d25ab347d7bf493f88d5c6ca538f7062334
webpwnized <webpwnized@gmail.com>
2024-10-30 18:58:51 -0400

    2.11.24 Update web services

--- src/classes/EncodingHandler.php
+++ src/classes/EncodingHandler.php
@@ -2,9 +2,9 @@
 
 class EncodingHandler {
 
-	/* ------------------------------------------
-	 * PROTECTED PROPERTIES
-	 * ------------------------------------------ */
+    /* ------------------------------------------
+     * PROTECTED PROPERTIES
+     * ------------------------------------------ */
     protected const cHTML = "HTML";
     protected const cXML = "XML";
     protected const cLDAP = "LDAP";
@@ -18,73 +18,85 @@ class EncodingHandler {
     protected const cXMLHEXENCPREFIX = "&#x";
     protected const cSEMICOLON = ";";
 
-	/* ------------------------------------------
-	 * CONSTRUCTOR METHOD
-	 * ------------------------------------------ */
-	public function __construct(){
+    /* ------------------------------------------
+     * CONSTRUCTOR METHOD
+     * ------------------------------------------ */
+    public function __construct(){
 
-	}// end function __construct()
+    }// end function __construct()
 
-	/* ------------------------------------------
- 	 * PRIVATE METHODS
- 	 * ------------------------------------------ */
+    /* ------------------------------------------
+     * PRIVATE METHODS
+     * ------------------------------------------ */
 
-	/* ------------------------------------------
-	 * PUBLIC METHODS
-	 * ------------------------------------------ */
-	private function encode(/*string*/ $pString, $pType) {
+    /* ------------------------------------------
+     * PUBLIC METHODS
+     * ------------------------------------------ */
+    private function encode(/*string*/ $pString, $pType) {
+        // Ensure $pString is a valid string to avoid warnings.
+        if (!is_string($pString) || empty($pString)) {
+            return ""; // Return an empty string if input is not valid.
+        }
 
-	    /*int*/ $lStrLen = strlen($pString);
-	    /*string*/ $lEncodedString = "";
-	    for ($i = 0; $i < $lStrLen; $i++){
-	        if (ctype_alnum($pString[$i])){
-	            $lEncodedString.=$pString[$i];
-	        }else{
-	            switch ($pType) {
-	                case self::cHTML || self::cXML:
-	                    $lEncodedString.=self::cXMLHEXENCPREFIX.strval(bin2hex($pString[$i])).self::cSEMICOLON;
-	                    break;
-	                case self::cURL:
-	                    $lEncodedString.=self::cPERCENT.strval(bin2hex($pString[$i]));
-	                    break;
-	                case self::cCSS || self::cLDAP:
-	                    $lEncodedString.=self::cBACKSLASH.strval(bin2hex($pString[$i]));
-	                    break;
-	                case self::cJS:
-	                    $lEncodedString.=self::cJAVASCRIPTHEXENCPREFIX.strval(bin2hex($pString[$i]));
-	                    break;
-	            }//end switch
-	        }//end if
-	    }//end for
-	    return $lEncodedString;
-	}//end private function
+        /*int*/ $lStrLen = strlen($pString);
+        /*string*/ $lEncodedString = "";
+        
+        for ($i = 0; $i < $lStrLen; $i++){
+            if (ctype_alnum($pString[$i])) {
+                $lEncodedString .= $pString[$i];
+            } else {
+                // Corrected switch case structure for better readability.
+                switch ($pType) {
+                    case self::cHTML:
+                    case self::cXML:
+                        $lEncodedString .= self::cXMLHEXENCPREFIX . strval(bin2hex($pString[$i])) . self::cSEMICOLON;
+                        break;
+                    case self::cURL:
+                        $lEncodedString .= self::cPERCENT . strval(bin2hex($pString[$i]));
+                        break;
+                    case self::cCSS:
+                    case self::cLDAP:
+                        $lEncodedString .= self::cBACKSLASH . strval(bin2hex($pString[$i]));
+                        break;
+                    case self::cJS:
+                        $lEncodedString .= self::cJAVASCRIPTHEXENCPREFIX . strval(bin2hex($pString[$i]));
+                        break;
+                    default:
+                        // Handle unsupported encoding types gracefully.
+                        throw new Exception("Unsupported encoding type: $pType");
+                }//end switch
+            }//end if
+        }//end for
 
-	public function encodeForLDAP(/*string*/ $pString) {
-	    return $this->encode($pString, self::cLDAP);
-	}//end private function
+        return $lEncodedString;
+    }//end private function
 
-	public function encodeForCSS(/*string*/ $pString) {
-	    return $this->encode($pString, self::cCSS);
-	}//end private function
+    public function encodeForLDAP(/*string*/ $pString) {
+        return $this->encode($pString, self::cLDAP);
+    }//end public function
 
-	public function encodeForHTML(/*string*/ $pString) {
-	    return $this->encode($pString, self::cHTML);
-	}//end private function
+    public function encodeForCSS(/*string*/ $pString) {
+        return $this->encode($pString, self::cCSS);
+    }//end public function
 
-	public function encodeForXML(/*string*/ $pString) {
-	    return $this->encode($pString, self::cXML);
-	}//end private function
+    public function encodeForHTML(/*string*/ $pString) {
+        return $this->encode($pString, self::cHTML);
+    }//end public function
 
-	public function encodeForXPath(/*string*/ $pString) {
-	    return $this->encode($pString, self::cXML);
-	}//end private function
+    public function encodeForXML(/*string*/ $pString) {
+        return $this->encode($pString, self::cXML);
+    }//end public function
 
-	public function encodeForJavaScript(/*string*/ $pString) {
-	    return $this->encode($pString, self::cJS);
-	}//end private function
+    public function encodeForXPath(/*string*/ $pString) {
+        return $this->encode($pString, self::cXML);
+    }//end public function
 
-	public function encodeForURL(/*string*/ $pString) {
-	    return $this->encode($pString, self::cURL);
-	}//end private function
+    public function encodeForJavaScript(/*string*/ $pString) {
+        return $this->encode($pString, self::cJS);
+    }//end public function
+
+    public function encodeForURL(/*string*/ $pString) {
+        return $this->encode($pString, self::cURL);
+    }//end public function
 
 }// end class
