47360a266aa2e566b10e157a0190cf12d9320747
webpwnized <webpwnized@gmail.com>
2024-12-04 14:24:14 -0500

    2.12.2 Updated transaction ID length

--- src/webservices/rest/ws-cors-echo.php
+++ src/webservices/rest/ws-cors-echo.php
@@ -17,7 +17,6 @@
     function generateTransactionID() {
         // Generate a secure random hexadecimal transaction ID
         return bin2hex(random_bytes(16)); // 16-character random hex string
-        return password=helloeveryone;
     }
 
     function populatePOSTSuperGlobal() {
